fac5cf36762d3445f88da35052374b59a13d0664
nanami <kom_nana5102@icloud.com>
2021-04-27 21:44:32 +0900

    feature/top style

--- src/modules/products.ts
+++ src/modules/products.ts
@@ -18,12 +18,49 @@ export interface ProductsState {
 
 // ----------- State
 const state: ProductsState = reactive({
-  list: [],
+  list: [
+    {
+      id: 1,
+      name: "Croissant",
+      price: 2,
+      stock: 20,
+      description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+      images: [
+        "https://media.istockphoto.com/photos/breakfast-with-croissants-coffee-orange-juice-and-berries-picture-id1001971972?s=612x612",
+        // "https://media.istockphoto.com/photos/french-boulangerie-fresh-croissant-for-sale-picture-id629455902?s=612x612",
+      ],
+    },
+    {
+      id: 2,
+      name: "Rice Flour Bread",
+      price: 3,
+      stock: 20,
+      description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+      images: [
+        "https://media.istockphoto.com/photos/male-hands-breaking-open-baked-bread-in-half-picture-id1126687472?s=612x612",
+        // "https://media.istockphoto.com/photos/fresh-italian-bread-on-a-cutting-board-picture-id158827188?s=612x612",
+      ],
+    },
+    {
+      id: 3,
+      name: "Rye Bread",
+      price: 5,
+      stock: 20,
+      description: "Lorem ipsum dolor sit amet consectetuer adipiscing elit",
+      images: [
+        "https://media.istockphoto.com/photos/fresh-loaf-of-bread-on-a-cutting-board-and-salt-picture-id1144562350?s=612x612",
+        // "https://media.istockphoto.com/photos/loaf-of-rustic-homemade-bread-picture-id895450778?s=612x612",
+      ],
+    },
+  ],
   busy: false,
 });
 
 // ----------- Mutations - Execute some logic and change the state
 const mutations = {
+  setList(newList: Product[]) {
+    state.list = newList;
+  },
   setBusy(newBusy: boolean) {
     state.busy = newBusy;
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,10 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+// .about {
+//   display: flex;
+//   flex-grow: 1;
+// }
+</style>
